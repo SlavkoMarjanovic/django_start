69f04ce1aa7feadb029bbaecc072ea9570445a22
Slavko <slavkopb@gmail.com>
2017-08-13 11:51:06 +0200

    13.8 proradili restaurant views

--- cfehome/src/cfehome/urls.py
+++ cfehome/src/cfehome/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-#from restaurant.views import HomeView, AboutView, ContentView
+from restaurant.views import restaurantlistview
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name ='home.html')),
-    url(r'^about$', TemplateView.as_view(template_name ='about.html')),
-    url(r'^contact$', TemplateView.as_view(template_name ='contact.html')),
+    url(r'^restaurant/$', restaurantlistview),
+    url(r'^about/$', TemplateView.as_view(template_name ='about.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name ='contact.html')),
 ]
--- cfehome/src/restaurant/views.py
+++ cfehome/src/restaurant/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView
-'''
+from .models import RestaurantLocation
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
-    def get_context_data(self, *args, **kwargs):
-        contex = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = random.randint(0, 1000000)
-        some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000)]
-        contex = {
-            "num": num,
-            "some_list": some_list
-        }
+#from django.views.generic import TemplateView
+def restaurantlistview(request):
+    template_name = "restaurant/restaurant_list.html"
+    query_set = RestaurantLocation.objects.all()
+    contex ={
+        "object_list" : query_set
+    }
+    return render(request, template_name, contex)
 
-        print(contex)
-        return contex
 
-class AboutView(TemplateView):
-    template_name = 'about.html'
-class ContentView(View):
-    def get(self, request, *args, **kwargs):
-        contents = {}
-        return render(request, "contact.html", contents)'''
+
+
+
+
+
 
 #    return HttpResponse(html_)
 # pozdrav
