7d23b46abf6d17efda8e29b05e21d36e3e48dda2
Slavko <slavkopb@gmail.com>
2017-08-15 00:23:35 +0200

    Post metod rešen

--- cfehome/src/restaurant/views.py
+++ cfehome/src/restaurant/views.py
@@ -1,9 +1,29 @@
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from .models import RestaurantLocation
+from .forms import RestorauntCreateForm
 from django.views.generic import TemplateView, ListView, DetailView
+
+
+def restaurant_createview(request):
+    form = RestorauntCreateForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            obj = RestaurantLocation.objects.create(
+                name = form.cleaned_data.get("name"),
+                location = form.cleaned_data.get("location"),
+                category = form.cleaned_data.get("category")
+            )
+        return HttpResponseRedirect("/restaurant/")
+    template_name = 'restaurant/form.html'
+    contex = {"form":form}
+    return render(request, template_name, contex)
+
+
+
+
 
 def restaurantlistview(request):
     template_name = "restaurant/restaurantlocation_list.html"
