56e6dcd8d6a754e8b4abc422b751d542f952667d
Slavko <slavkopb@gmail.com>
2017-08-16 23:06:53 +0200

    Radna verzija proba

--- cfehome/src/restaurant/models.py
+++ cfehome/src/restaurant/models.py
@@ -16,6 +16,7 @@ class RestaurantLocation(models.Model):
     @property
     def title(self):
         return self.name
+    #pozdrav
 def rl_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
