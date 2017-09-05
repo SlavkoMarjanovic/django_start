60d182ce8bbf7bbd75680301ab78c7fbcdaeec94
Slavko <slavkopb@gmail.com>
2017-09-06 00:05:54 +0200

    More with detail on profiles app

--- cfehome/src/cfehome/urls.py
+++ cfehome/src/cfehome/urls.py
@@ -19,13 +19,14 @@ from django.contrib.auth.views import LoginView
 from restaurant.views import Restaurantlistview, RestaurantDetailView, RestaurantCreateView
 
 
+
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name ='home.html'), name= "home"),
     url(r'^login/$',LoginView.as_view(),name='login'),
-    url(r'^restaurant/', include("restaurant.url", namespace="restaurant")),
+#    url(r'^restaurant/', include("restaurant.url", namespace="restaurant")),
     url('^accounts/', admin.site.urls),
     url(r'^items/', include("menus.urls", namespace="menus")),
     url(r'^u/', include("profiles.urls", namespace="profiles")),
--- cfehome/src/profiles/views.py
+++ cfehome/src/profiles/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
+from django.http import Http404
 from django.contrib.auth import get_user_model
 
-user = get_user_model()
+User = get_user_model()
 class ProfilesDetailView(DetailView):
-    queryset = user.objects.filter(is_active =True)
+
+    template_name = 'profiles/user.html'
+
+    def get_object(self):
+        user_name = self.kwargs.get("username")
+        if user_name is None:
+            raise Http404
+        return get_object_or_404(User, username__iexact=user_name, is_active =True)
 
 
 # Create your views here.
